404f3322adedbbe8ba5df87c2c4dc26622fe86fb
jakobdubeau <jakobdubeau@gmail.com>
2026-01-16 00:51:59 -0500

    add embed to component

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { ChevronLeftIcon } from "@heroicons/react/24/solid";
 import SpotifyEmbed from './ui/SpotifyEmbed';
 
@@ -7,17 +7,34 @@ import SpotifyEmbed from './ui/SpotifyEmbed';
 // playlist and open in spotify link
 
 const Playlist = ({ onBack }) => {
+
+  const [playlistId, setPlaylistId] = useState(null)
+
+  useEffect(() => {
+    const generatePlaylist = async () => {
+      const res = await fetch("/api/spotify/generate", {
+        method: "POST",
+      })
+      if (res.ok) {
+        const data = await res.json()
+        setPlaylistId(data.playlist.id)
+      }
+    }
+    generatePlaylist()
+  }, [])
+
   return (
-    <section>
+    <section className="flex flex-col flex-1 items-center justify-center">
       <button
         onClick={onBack}
-        className="absolute top-11 left-12 flex items-center font-semibold text-2xl text-[#ADB7BE] hover:text-white cursor-pointer transition-all hover:scale-105 duration-200"
+        className="absolute top-10 left-12 flex items-center font-semibold text-2xl text-[#ADB7BE] hover:text-white cursor-pointer transition-all hover:scale-105 duration-200"
       >
         <ChevronLeftIcon className="w-7 h-7" />
         <span>
           back
         </span>
       </button>
+      {playlistId && <SpotifyEmbed playlistId={playlistId} />}
     </section>
   )
 }
