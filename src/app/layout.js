79da41ed05816cbbd5fb2545aa3fe0d52317b921
jakobdubeau <jakobdubeau@gmail.com>
2026-02-15 14:17:08 -0500

    move stuff around

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
+import { UserIcon } from "@heroicons/react/24/outline";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,17 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
       >
-        {children}
+        <main className="flex min-h-screen flex-col bg-black bg-[radial-gradient(#2a2030_1px,transparent_1px)] bg-size-[16px_16px]">
+          <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,transparent_50%,rgba(0,0,0,0.25)_75%)] pointer-events-none"></div>
+          <div className="container mt-24 mx-auto px-12 pt-4 flex flex-col flex-1">
+            <button
+              className="absolute right-12 top-10 text-[#ADB7BE] hover:text-white cursor-pointer transition-all hover:scale-105 duration-200"
+            >
+              <UserIcon className="w-7 h-7 stroke-2"/>
+            </button>
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import HeroSection from "../components/HeroSection";
 import Playlist from "../components/Playlist";
-import { UserIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
 
@@ -46,26 +45,16 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col bg-black bg-[radial-gradient(#2a2030_1px,transparent_1px)] bg-size-[16px_16px]">
-      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,transparent_50%,rgba(0,0,0,0.25)_75%)] pointer-events-none"></div>
-      <div className="container mt-24 mx-auto px-12 pt-4 flex flex-col flex-1">
-        <button
-          className="absolute right-12 top-10 text-[#ADB7BE] hover:text-white cursor-pointer transition-all hover:scale-105 duration-200"
-        >
-          <UserIcon className="w-7 h-7 stroke-2"/>
-        </button>
-        <div
-          className={`flex flex-col flex-1 transition-opacity duration-200 ease-out ${
-            transitioning ? "opacity-0" : "opacity-100"
-          }`}
-        >
-          {!showPlaylist ? (
-            <HeroSection onGen={handleCreate} /> 
-          ) : (
-            <Playlist onBack={goBack} />
-          )}
-        </div>
-      </div>
-    </main>
+    <div
+      className={`flex flex-col flex-1 transition-opacity duration-200 ease-out ${
+        transitioning ? "opacity-0" : "opacity-100"
+      }`}
+    >
+      {!showPlaylist ? (
+        <HeroSection onGen={handleCreate} />
+      ) : (
+        <Playlist onBack={goBack} />
+      )}
+    </div>
   );
 }
